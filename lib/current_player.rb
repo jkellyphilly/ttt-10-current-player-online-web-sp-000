b82222d1da843e9c61412f56ff8ccbc715d7cc76
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-19 21:24:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,12 @@ def turn_count(board)
   counter
 end
 
-# divide by 2; if mod is 0, then an even number of turns have been played and thus it is X's turn
-#if counter % 2 == 0
-  # it's X's turn
-#  # it's O's turn
+def current_player(board)
+  # if the count divided by 2 has a remainder of zero; it's X's turn
+  if (turn_count(board) % 2) == 0
+    return "X"
+  # otherwise, it's O's turn
+  else
+    return "O"
+  end
+end
